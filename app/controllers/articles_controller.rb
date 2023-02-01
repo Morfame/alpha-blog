d42fca216492ac9e22fa3c526fd61754c29386a9
Morfame <baramorf@gmail.com>
2023-02-01 14:16:02 +0000

    Created listing table from DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   def show
-    #binding.break
     @article = Article.find(params[:id]) #the @ sign makes variable Public (instance is rails name)
   end
+
+  def index
+    @articles = Article.all #the @ sign makes variable Public (instance is rails name)
+  end
+
 end
