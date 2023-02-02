f59ca1c63a9d422c0c6fa6ab6fe673622993086e
Morfame <baramorf@gmail.com>
2023-02-02 15:52:30 +0000

    Flash and Params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,38 @@
 class ArticlesController < ApplicationController
   def show
-    @article = Article.find(params[:id]) #the @ sign makes variable Public (instance is rails name)
+    @article = Article.find(params[:id])
   end
 
   def index
-    @articles = Article.all #the @ sign makes variable Public (instance is rails name)
+    @articles = Article.all
   end
 
   def new
-
+    @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to (@article) #shortened path from article_path(@article)
+    if @article.save
+      redirect_to @article 
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
 end
